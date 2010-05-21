bc7e3b8e7cff6f5d589edac542641e0353387ad6
Konstantin Alekseev <konstantin@smtp.com>
2010-05-21 21:25:32 +0300

    trains commited

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -5,11 +5,13 @@ class MapController < ApplicationController
     gg = GoogleGeocode.new YAML.load_file(RAILS_ROOT + "/config/gmaps_api_key.yml")[ENV['RAILS_ENV']]
     loc = gg.locate "Kyiv"
     
+    coords = Line.find_by_color('red').stations.map{|s| [s.lat, s.lng]}
+    
     @map = GMap.new("map_div")
     @map.control_init(:small => true, :large_map => true) #add :large_map => true to get zoom controls
-    @map.center_zoom_init([loc.latitude, loc.longitude],12)
+    @map.center_zoom_init([loc.latitude, loc.longitude],11)
     @map.overlay_init(GMarker.new([loc.latitude, loc.longitude], :title => "xoi", :info_bubble => loc.address))
-    @map.overlay_init(GMarker.new([50.44407331720806, 30.505846738815308], :title => "Train 1", :info_bubble => "TRRRRRAAAAAIN"))
+    @map.overlay_init(GMarker.new(coords[rand(coords.size)], :title => "Train 1", :info_bubble => "TRRRRRAAAAAIN"))
   end
   
   def frame
